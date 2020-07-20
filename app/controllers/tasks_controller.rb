266f3949f93c0746bc22112d9ab1ed5da3cc7243
halki <s.halki0203@icloud.com>
2020-07-21 07:38:31 +0900

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,18 @@ class TasksController < ApplicationController
   def new
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save! #!マークをつける意味？
+    redirect_to tasks_url, notice:"タスクを「#{task.name}」登録しました" #pathとurlの違い？、redirect_toとrenderの違いをはっきりさせよう
+  end
+
   def edit
+  end
+
+  private
+  
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
