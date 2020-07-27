2ac17a2770eaaac5a5e3d073e9ab81e7c0532aaa
halki <s.halki0203@icloud.com>
2020-07-27 21:54:50 +0900

    up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.authentecate(session_params[:password])
+      session[:user_id] = user.user.id
+      redirect_to root_path, notice: 'ログインしました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+  
 end
