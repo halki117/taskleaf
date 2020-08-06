7e4f6a12f0877b1d876b402dd50924505f3e00a2
halki <s.halki0203@icloud.com>
2020-08-06 22:05:39 +0900

    Update tasks_spec.rb

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+
+
     before do
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+      # user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      # FactoryBot.create(:task, name: '最初のタスク', user: user_a)
     end
 
     context 'ユーザーAでログインしている時' do
